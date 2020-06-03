daafbc5f9eb0de0510f6a71504b717c14c58c5d9
gfoisy <gfoisy@u.rochester.edu>
2020-06-03 15:54:24 -0400

    updates
    updates

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,21 @@ document.getElementById('divider').onclick = function changeContent() {
 
 };
 
-alert("worked");
+// alert("worked");
+
+const clicker=document.getElementById('clicker');
+const current=document.getElementById('current');
+const why=document.getElementById('why-this');
+let menuoff=true;
+
+
+clicker.onclick= function changeColor(){
+  if (menuoff){
+  clicker.style.backgroundColor='red';
+  current.style='margin:auto; padding-left:2rem;';
+  why.style='display:flex; background-color:red;';
+  menuoff=false;
+}else{
+  clicker.style.backgroundColor='';
+  }
+}
