2fb0e95fedae9ac51428ba7f89826152a8e27633
gfoisy <gfoisy@u.rochester.edu>
2020-08-03 15:33:28 -0400

    working copy

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const current=document.getElementById('current');
 const compressed=document.getElementById('compressed');
 const afterintro=document.getElementById('afterintro');
 const html=document.getElementById('html');
+const bigContainer=document.getElementById('big');
+const body=document.getElementById('body')
 let menuoff=true;
 
 var mediaQuery=window.matchMedia("(min-width:800px)");
@@ -10,12 +12,16 @@ var mediaQuery=window.matchMedia("(min-width:800px)");
 
 clicker.onclick= function toggleMenu(){
   if (menuoff){
-  compressed.style='display: flex;';
+  compressed.style= 'display: flex;';
+  clicker.syle= 'color: #99BCA6;';
+  body.style= 'overflow-y: hidden;';
   menuoff=false;
 }else{
-  current.style='';
-  compressed.style='';
-  menuoff=true;
+  clicker.syle= 'color: #EBE3DE;';
+  current.style= '';
+  compressed.style= '';
+    body.style= 'overflow-y: scroll;';
+  menuoff= true;
   }
 };
 
