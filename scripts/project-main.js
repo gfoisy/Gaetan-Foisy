683519633f6c509da80bd012b45bc21635acb4b5
gfoisy <61033315+gfoisy@users.noreply.github.com>
2020-08-24 13:26:32 -0400

    Project titles finished
    Project title transition finished and working

--- scripts/project-main.js
+++ scripts/project-main.js
@@ -4,11 +4,11 @@
 function mouseOver(x){
   var y=x;
   document.getElementsByClassName('projtag')[y].style= "background-color: #99BCA6";
-
+  document.getElementsByClassName('image')[y].style= "border-bottom-right-radius: 0;  border-bottom-left-radius: 0;";
 }
 
 function mouseOut(x){
   var y=x;
   document.getElementsByClassName('projtag')[y].style= "background-color: none";
-
+  document.getElementsByClassName('image')[y].style= "border-bottom-right-radius: 10px;  border-bottom-left-radius: 10px;";
 }
