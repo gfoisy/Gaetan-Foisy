c50785f7362353056a79bc350816d53c67ea0c58
gfoisy <61033315+gfoisy@users.noreply.github.com>
2020-08-24 13:05:37 -0400

    Project page Js highlight
    Titles on the project page now highlght

---
+++ scripts/project-main.js
@@ -0,0 +1,14 @@
+
+
+
+function mouseOver(x){
+  var y=x;
+  document.getElementsByClassName('projtag')[y].style= "background-color: #99BCA6";
+
+}
+
+function mouseOut(x){
+  var y=x;
+  document.getElementsByClassName('projtag')[y].style= "background-color: none";
+
+}
